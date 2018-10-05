2684b19a59354574551606a5ef0b74e790ef7698
Austin  Burke <aburk3@gmail.com>
2018-10-05 01:09:50 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,34 +1,24 @@
 ## code your solution here.
 class Cat
-  def name(name_of_cat)
-    attr_accessor :name
-    puts "#{name_of_cat}.capitalize"
 
-  end
+    attr_accessor :name()
+    def name(name_of_cat)
+      puts "#{name_of_cat}.capitalize"
+    end
 
-  def meow
     attr_accessor :meow
-    puts "meow!"
-  end
+    def meow
+      puts "meow!"
+    end
 end
 
+class Cat
+  def initialize(name)
+    @name = name
+  end
 
-# class Cat
-#
-#   def name=(name)
-#     @name = name
-#   end
-#
-#   def name
-#     puts "#{name}.capitalize"
-#   end
-#
-#   def name=(meow)
-#     @meow = meow
-#   end
-#
-#   def meow
-#     puts "meow!"
-#   end
-#
-# end
+	def meow
+    puts "meow"
+	end
+
+end
